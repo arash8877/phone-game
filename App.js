a3ba73b8482107f108787afa07a87fff9bd141f6
Arash <arash8877@gmail.com>
2024-03-17 20:41:06 +0100

    make the confirm button dynamic

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, ImageBackground } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
-import {LinearGradient} from 'expo-linear-gradient';
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
   return (
-    <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
-      <StartGameScreen />
+    <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
+      <ImageBackground
+        source={require("./assets/images/Dice.jpg")}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 }
 
-
 const styles = StyleSheet.create({
-   rootScreen: {
+  rootScreen: {
     flex: 1,
-    backgroundColor: '#ddb52f',
-   }
-})
+  },
+  backgroundImage: {
+    opacity: 0.15,
+  }
+});
--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,20 +1,18 @@
 import { View, Text, Pressable, StyleSheet } from "react-native";
 
-const PrimaryButton = ({ children }) => {
-  const pressHandler = () => {
-    console.log("button is pressed!");
-  };
+const PrimaryButton = ({ children, onPress }) => {
+
 
   return (
     <View style={styles.buttonOuterContainer}>
       <Pressable
-        onPress={pressHandler}
-        android_ripple={{ color: "yellow" }}
         style={({ pressed }) =>
           pressed
             ? [styles.buttonInnerContainer, styles.pressed]
             : styles.buttonInnerContainer
         }
+        onPress={onPress}
+        android_ripple={{ color: "yellow" }}
       >
         <Text style={styles.buttonText}>{children}</Text>
       </Pressable>
