85e22c29a5d8f98e9034a7b596b2e3f3cbca1b24
Arash <arash8877@gmail.com>
2024-03-18 18:52:50 +0100

    using state to switch the page

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import { Text } from "react-native";
+
 
 const GameScreen = () => {
   return (
-    <div>GameScreen</div>
+    <Text>Game screen</Text>
   )
 }
 
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { TextInput, View, StyleSheet, Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
-const StartGameScreen = () => {
+const StartGameScreen = ({onPickNumber}) => {
   const [enteredNumber, setEnteredNumber] = useState("");
 
   const numberInputHandler = (enteredText) => {
@@ -24,7 +24,7 @@ const StartGameScreen = () => {
       );
       return;
     }
-    console.log('valid number!!!')
+    onPickNumber(chosenNumber);
   };
 
   return (
