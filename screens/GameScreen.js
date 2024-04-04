649b1d07a861306e59259cffd124b8d29b00e74e
Arash <arash8877@gmail.com>
2024-04-04 21:23:39 +0200

    modified GameScreen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,10 +1,25 @@
-import { Text } from "react-native";
+import { View ,Text, StyleSheet } from "react-native";
 
 
 const GameScreen = () => {
   return (
-    <Text>Game screen</Text>
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      {/* GUESS */}
+      <View>
+        <Text>Higher or Lower?</Text>
+        {/* + - */}
+      </View>
+      {/* <View>LOG ROUNDS</View> */}
+    </View>
   )
 }
 
-export default GameScreen
+export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 12,
+  }
+})
