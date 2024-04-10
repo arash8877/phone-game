83cf04b71244a801cb0ede9054e89869ce11afc8
Arash <arash8877@gmail.com>
2024-04-10 13:21:47 +0200

    created GameOverScreen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,9 +1,32 @@
-import { Text } from "react-native"
+import { View, Image, StyleSheet } from "react-native";
+import Title from "../components/ui/Title";
+import Colors from "../components/constants/colors";
 
 const GameOverScreen = () => {
   return (
-    <Text>Game is over!</Text>
-  )
-}
+    <View>
+      <Title>GAME OVER!</Title>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={require("../assets/images/success.png")} />
+      </View>
+    </View>
+  );
+};
 
-export default GameOverScreen
+export default GameOverScreen;
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    borderRadius: 200,
+    height: 400,
+    width: 400,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    overflow: 'hidden',
+    margin: 36,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  }
+});
