ee4eda98bab60d9956496e5e81c63260d7ea0ae6
Arash <arash8877@gmail.com>
2024-04-10 10:18:53 +0200

    adding icons

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -5,6 +5,7 @@ import NumberContainer from "../components/game/NumberContainer";
 import PrimaryButton from "../components/ui/PrimaryButton";
 import Card from "../components/ui/Card";
 import InstructionText from "../components/ui/InstructionText";
+import { Entypo } from '@expo/vector-icons';
 
 const generateRandomBetween = (min, max, exclude) => {
   const rndNum = Math.floor(Math.random() * (max - min)) + min;
@@ -61,12 +62,12 @@ const GameScreen = ({ userNumber, onGameOver }) => {
         <View style={styles.buttonsContainer}>
           <View style={styles.buttonContainer}>
             <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
-              -
+              <Entypo name="minus" size={24} color="white"/ >
             </PrimaryButton>
             </View>
             <View style={styles.buttonContainer}>
             <PrimaryButton onPress={nextGuessHandler.bind(this, "greater")}>
-              +
+            <Entypo name="plus" size={24} color="white"/ >
             </PrimaryButton>
           </View>
         </View>
